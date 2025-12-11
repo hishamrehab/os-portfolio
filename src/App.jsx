49a3c53f4f811e961d99e945aa945c741eee4f9f
hishamrehab <hishamrehab30@gmail.com>
2025-12-11 15:24:54 +0200

    Add index.js file to enahnce importaing fiels

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-
+import { Navbar , Welcome } from "./components"
 const App = () => {
   return (
   <main>
     <Navbar />
+    <Welcome />
   </main>
   )
 }
